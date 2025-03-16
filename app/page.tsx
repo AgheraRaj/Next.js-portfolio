3d32f76714e2ab0b48fa764e384aaa946459adde
AgheraRaj <rajaghera2004@gmail.com>
2025-03-16 12:18:37 +0530

    add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/sections/Hero'
+import Skills from '@/components/sections/Skills'
 import React from 'react'
 
 const Home = () => {
   return (
     <main className='min-h-screen bg-background'>
       <Hero />
+      <Skills />
     </main>
   )
 }
